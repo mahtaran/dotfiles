827aecd3c77958cb8e43f4a9f12943ac23935a3c
Luka Leer <luka.leer@gmail.com>
2024-05-01 02:50:29 +0200

    📌 Specify version of Lanzaboote
    Signed-off-by: Luka Leer <luka.leer@gmail.com>

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote";
+      url = "github:nix-community/lanzaboote/v0.3.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

ab8a6c730199e68f3af5b57e24f7c5dc6958d0ca
Luka Leer <luka.leer@gmail.com>
2024-05-04 03:14:01 +0200

    🔒️ Securely mount ESP
    Signed-off-by: Luka Leer <luka.leer@gmail.com>

--- module/disko/single-disk.nix
+++ module/disko/single-disk.nix
@@ -15,7 +15,7 @@
                 format = "vfat";
                 mountpoint = "/boot";
                 mountOptions = [
-                  "defaults"
+                  "umask=0077"
                 ];
               };
             };
